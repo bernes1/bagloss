c42a7a31ca0500d994bfc08ccae2ea78a918c31a
bernes1 <37117219+bernes1@users.noreply.github.com>
2026-03-05 00:57:51 +0100

    inital draft

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import feedparser
+
+def send_message():
+    pass
+
+def feed():
+    d = feedparser.parse('https://www.githubstatus.com/history.atom')
+    
+    github_events = []
+
+    # iterate over parsed entries to collect titles
+    for entry in d.entries:
+        github_events.append(f"{entry.title} at {entry.updated}")
+
+    return github_events
+
 def main():
-    print("Hello from bagloss!")
+    for i in feed():
+        print(i)
 
 
 if __name__ == "__main__":
