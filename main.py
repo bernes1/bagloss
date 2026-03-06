94883df07611d2f2a9147e2e85e1e6fda117882c
bernes1 <37117219+bernes1@users.noreply.github.com>
2026-03-06 01:23:10 +0100

    well add a working version

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
+import os
 import feedparser
+import time
+import requests
+from datetime import datetime, timedelta, timezone
 
-def send_message():
-    pass
+token = os.getenv("PUSHOVER_TOKEN")
+userkey = os.getenv("PUSHOVER_USER_KEY")
 
-def feed():
-    d = feedparser.parse('https://www.githubstatus.com/history.atom')
-    
-    github_events = []
 
-    # iterate over parsed entries to collect titles
-    for entry in d.entries:
-        github_events.append(f"{entry.title} at {entry.updated}")
+def send_message(event_title,updated_time):
+    time = datetime.fromisoformat(updated_time).strftime("%H:%M:%S %d-%m-%Y ")
+    event_message = f"New event: {event_title, time}"
+    print(event_message)
 
-    return github_events
+    r = requests.post("https://api.pushover.net/1/messages.json", data = {
+        "token": token,
+        "user": userkey,
+        "message": event_message
+    },headers={"Content-type": "application/x-www-form-urlencoded"})
+
+    return r.text
+
+
+
+def is_one_day_old(timestamp: str):
+    # checks if is less than one day old
+    input_time = datetime.fromisoformat(timestamp)
+    current_time = datetime.now(timezone.utc)
+    time_diff = current_time - input_time
+    return time_diff <= timedelta(days=1)
+
+
+
 
 def main():
-    for i in feed():
-        print(i)
+    seen_titles = set()
+    while True:
+        d = feedparser.parse('https://www.githubstatus.com/history.atom')
 
+        ## ignore anything that is older than one day 
+        for entry in d.entries:
+            if entry.title not in seen_titles:
+                if is_one_day_old(entry.updated) == True: 
+                    seen_titles.add(entry.title)
+                    send_message(entry.title, entry.updated)
+        time.sleep(60)  # Check every 60 seconds
 
 if __name__ == "__main__":
     main()
