715fd587d8982b00416150de92b70e9e58952501
bernes1 <37117219+bernes1@users.noreply.github.com>
2026-03-06 02:17:30 +0100

    Add title to notification and  add link for incident aaaand show last update in local timezone

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import feedparser
 import time
 import requests
 from datetime import datetime, timedelta, timezone
+from dateutil import tz
 
 token = os.getenv("PUSHOVER_TOKEN")
 userkey = os.getenv("PUSHOVER_USER_KEY")
+user_tz = os.getenv("USER_TZ")
 
-
-def send_message(event_title,updated_time):
-    time = datetime.fromisoformat(updated_time).strftime("%H:%M:%S %d-%m-%Y ")
-    event_message = f"New event: {event_title, time}"
+def send_message(event_title:str,updated_time:str,link:str) -> str:
+    to_zone = tz.gettz(user_tz)
+    time = datetime.fromisoformat(updated_time).astimezone(to_zone).strftime("%H:%M:%S | %d-%m-%Y ")
+    event_message = f"New Github Incident: {event_title}\nDate: {time}\nLink: {link}"
     print(event_message)
 
     r = requests.post("https://api.pushover.net/1/messages.json", data = {
         "token": token,
         "user": userkey,
-        "message": event_message
+        "message": event_message,
+        "title":"Github Incident",
+        "sound":"cashregister"
     },headers={"Content-type": "application/x-www-form-urlencoded"})
 
     return r.text
@@ -43,7 +47,7 @@ def main():
             if entry.title not in seen_titles:
                 if is_one_day_old(entry.updated) == True: 
                     seen_titles.add(entry.title)
-                    send_message(entry.title, entry.updated)
+                    send_message(entry.title, entry.updated, entry.link)
         time.sleep(60)  # Check every 60 seconds
 
 if __name__ == "__main__":
